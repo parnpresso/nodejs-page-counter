f71950c826f20db86654b0b264ae77c962840abe
Parn Boonyamanee <parnpresso@gmail.com>
2019-07-14 12:07:11 +0700

    Write to file when API called

--- app/page-counter/app.js
+++ app/page-counter/app.js
@@ -3,6 +3,18 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.use('/page-viewed', (req, res) => {
+  const fs = require('fs');
+  fs.writeFile("../counter.txt", "Hey there!", function(err) {
+    if(err) {
+      return console.log(err);
+    }
+    console.log("The file was saved!");
+    return res.status(200).json({});
+  });
+  }
+);
+
 app.listen(port, () => {
-    console.log(`Express server listening on port ${port}`);
+  console.log(`Express server listening on port ${port}`);
 });
